3d094ec1ad23947a63ab1a38bc9fbf4f74d36549
Ondrej Antl <oaspamz@gmail.com>
2020-12-01 19:19:42 +0100

    version 1
    added abilitz of reading txt files

---
+++ FileReadingLib/FileReader.cs
@@ -0,0 +1,36 @@
+﻿using FileReadingLib.Interfaces;
+using System;
+using System.IO;
+
+namespace FileReadingLib
+{
+    /// <inheritdoc/>
+    public class FileReader : IFileReader
+    {
+        /// <inheritdoc/>
+        public string ReadTextFile(string path)
+        {
+            if (!path.EndsWith(".txt"))
+            {
+                throw new ArgumentException("Provided file have not correct format. Required file format is .txt.");
+            }
+
+            var content = ReadFile(path);
+
+            return content;
+        }
+
+        /// <summary>
+        /// Reads a file content.
+        /// </summary>
+        /// <param name="path">Path to the file.</param>
+        /// <returns>Plain file content.</returns>
+        private string ReadFile(string path)
+        {
+            using (var sr = new StreamReader(path))
+            {
+                return sr.ReadToEnd();
+            }
+        }
+    }
+}
---
+++ FileReadingLib/Interfaces/IFileReader.cs
@@ -0,0 +1,13 @@
+﻿namespace FileReadingLib.Interfaces
+{
+    // Provides methods for reading files
+    public interface IFileReader
+    {
+        /// <summary>
+        /// Method for reading text file.
+        /// </summary>
+        /// <param name="path">Path to file to be read.</param>
+        /// <returns>Content of the file.</returns>
+        string ReadTextFile(string path);
+    }
+}
