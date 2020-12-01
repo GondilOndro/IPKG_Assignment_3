59b9c39eba605a1e12529656b5214f4fac8b293f
Ondrej Antl <oaspamz@gmail.com>
2020-12-01 19:21:45 +0100

    version 2
    added functionality for reading xml files

--- FileReadingLib/FileReader.cs
+++ FileReadingLib/FileReader.cs
@@ -1,6 +1,7 @@
 ﻿using FileReadingLib.Interfaces;
 using System;
 using System.IO;
+using System.Xml.Linq;
 
 namespace FileReadingLib
 {
@@ -20,6 +21,19 @@ namespace FileReadingLib
             return content;
         }
 
+        /// <inheritdoc/>
+        public string ReadXmlFile(string path)
+        {
+            if (!path.EndsWith(".xml"))
+            {
+                throw new ArgumentException("Provided file have not correct format. Required file format is .xml.");
+            }
+
+            var content = ReadFile(path);
+
+            return FormatXml(content);
+        }
+
         /// <summary>
         /// Reads a file content.
         /// </summary>
@@ -30,6 +44,24 @@ namespace FileReadingLib
             using (var sr = new StreamReader(path))
             {
                 return sr.ReadToEnd();
+            }
+        }
+
+        /// <summary>
+        /// Formats content of XML file as a pretty print XML.
+        /// </summary>
+        /// <param name="xmlContent">Plain content of XML file.</param>
+        /// <returns>Formated XML content.</returns>
+        private string FormatXml(string xmlContent)
+        {
+            try
+            {
+                XDocument xmlDoc = XDocument.Parse(xmlContent);
+                return xmlDoc.ToString();
+            }
+            catch (Exception e)
+            {
+                throw new Exception($"Parsing XML file failed: {e}");
             }
         }
     }
--- FileReadingLib/Interfaces/IFileReader.cs
+++ FileReadingLib/Interfaces/IFileReader.cs
@@ -9,5 +9,12 @@
         /// <param name="path">Path to file to be read.</param>
         /// <returns>Content of the file.</returns>
         string ReadTextFile(string path);
+
+        /// <summary>
+        /// Method for reading XML file.
+        /// </summary>
+        /// <param name="path">Path to file to be read.</param>
+        /// <returns>Formated content of the XML file.</returns>
+        string ReadXmlFile(string path);
     }
 }
